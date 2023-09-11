6555f944b37ff9b3671ec4237f2e8ca1a72ea7ae
znimator <zmeicreative@gmail.com>
2023-09-11 10:12:04 +0300

    photos

---
+++ lab2/something.py
@@ -0,0 +1,4 @@
+x = int(input("X: "))
+if x < 5:
+    print(1)
+elif x > 5 and 
--- lab2/task2/solution.py
+++ lab2/task2/solution.py
@@ -1,11 +1,8 @@
-h,m,s = float(input("Hours: ")), float(input("Minutes: ")), float(input("Seconds: "))
+h = float(input("Hours: "))
+m = float(input("Minutes: ")) 
+s = float(input("Seconds: "))
 
 MaxTime = 43200
 totalSeconds = (h * 60 * 60) + (m * 60) + (s)
-
-#Всего секунд может быть 43200,
-#Переводим заданное число в секунды
-#находим процент используя totalSeconds/MaxTime
-#далее 360 градусов умножаем на тот же самый процент
 
 print(360 * totalSeconds/MaxTime)
