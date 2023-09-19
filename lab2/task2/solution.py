f862dd87c7ae8a8da1a157570043d78e2f246b52
znimator <zmeicreative@gmail.com>
2023-09-19 10:10:07 +0300

    add

--- lab2/task2/solution.py
+++ lab2/task2/solution.py
@@ -1,8 +1,12 @@
+import math
 h = float(input("Hours: "))
 m = float(input("Minutes: ")) 
 s = float(input("Seconds: "))
 
 MaxTime = 43200
 totalSeconds = (h * 60 * 60) + (m * 60) + (s)
+if totalSeconds > MaxTime:
+    totalSeconds -= MaxTime * math.floor(totalSeconds/MaxTime)
+
 
 print(360 * totalSeconds/MaxTime)
