d9e1bce9f081adf7339d890ad4b8c31e3f28b1f6
deepxd46 <144063051+deepxd46@users.noreply.github.com>
2023-10-09 16:41:20 +0300

    add

--- labb5/task2.py
+++ labb5/task2.py
@@ -1,9 +1,15 @@
-a = int(input())
-b = int(input())
-c = a
-while c <= b:
-    print(1.8 * c + 32)
-    c += 1
+a = int(input("Введите начальную температуру в градусах Цельсия (C): "))
+b = int(input("Введите конечную температуру в градусах Цельсия (C): "))
+
+print("Таблица перевода температуры из C в F:")
+print("Celsius (C)\tFahrenheit (F)")
+
+while a <= b:
+    fahrenheit = 1.8 * a + 32
+    a += 1
+print(f"{a}\t\t{fahrenheit}")
 
 
-    
+
+
+
--- test.py
+++ test.py
@@ -1,1 +1,16 @@
-print("A")
+input_string = input("Введите строку: ")
+
+# Проверяем, что длина строки четная
+if len(input_string) % 2 == 0:
+    first_char = input_string[0] # Получаем первый символ
+    modified_string = first_char # Начинаем с первого символа
+
+# Проходим по остальным символам строки и добавляем их в измененную строку,
+# только если они не равны первому символу
+    for char in input_string[1:]:
+        if char != first_char:
+            modified_string += char
+
+    print("Измененная строка:", modified_string)
+else:
+    print("Длина строки нечетная, не требуется удаление.")
