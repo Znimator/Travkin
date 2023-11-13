2914f447f98bbb8ea79d4e3bb2a337751bd3792c
znimator <zmeicreative@gmail.com>
2023-11-13 11:07:08 +0300

    fixed labs

--- Labs/7 (Работа со строками в языке Python)/Solution 1.py
+++ Labs/7 (Работа со строками в языке Python)/Solution 1.py
@@ -4,10 +4,16 @@
 
 input_string = input("Введите строку: ")
 
-if len(input_string) % 2 == 0:
-    first_char = input_string[0]
-    result_string = first_char + input_string[1:].replace(first_char, '')
+if len(input_string) % 2 != 0:
+    print("Длинна строки не четная")
+    exit()
 
-    print("Результат:", result_string)
-else:
-    print("Длина строки не является четным числом.")
+first_char = input_string[0]
+
+new_string = first_char
+for i in input_string:
+    if i != first_char:
+        print(i)
+        new_string += i
+
+print("Результат:", new_string)
